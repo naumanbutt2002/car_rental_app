025de40277a8236adfd4853a27a7a823ba8891d4
naumanbutt2002 <nauman100percent@gmail.com>
2024-08-17 08:38:49 -0400

    Onboarding Screen

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:car_rental_app/firebase_options.dart';
+import 'package:car_rental_app/onboarding_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Car Rental App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: OnboardingPage(),
+    );
+  }
+}
