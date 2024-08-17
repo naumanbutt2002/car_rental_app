be918ceefef910bba31c12456487e2da578b5a48
naumanbutt2002 <nauman100percent@gmail.com>
2024-08-17 09:48:29 -0400

    Home Screen Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:car_rental_app/firebase_options.dart';
-import 'package:car_rental_app/onboarding_page.dart';
+import 'package:car_rental_app/presentation/pages/car_list_screen.dart';
+import 'package:car_rental_app/presentation/pages/onboarding_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
--- lib/presentation/pages/onboarding_page.dart
+++ lib/presentation/pages/onboarding_page.dart
@@ -1,3 +1,4 @@
+import 'package:car_rental_app/presentation/pages/car_list_screen.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingPage extends StatelessWidget {
@@ -47,7 +48,12 @@ class OnboardingPage extends StatelessWidget {
                     width: 320,
                     height: 54,
                     child: ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => CarListScreen()));
+                      },
                       style: ElevatedButton.styleFrom(
                         foregroundColor: Colors.black,
                         backgroundColor: Colors.white,
